6dc6c5e16bd25e41de8be184666687716f74e472
Annisa Lianda <52541134+AnnisaRizki30@users.noreply.github.com>
2025-01-27 03:18:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 import torch
+import numpy as np
 from PIL import Image
 from collections import OrderedDict
 from object_replacer.src import models
